f81b5a71eda224250ec2e5f05d01bd21e5934afb
Oscar <diazoscar088@gmail.com>
2026-03-02 21:49:09 +0100

    create CSS folder structure, fonts, header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import {Header} from './components/header';
 
-function App() {
+export function App():React.FC {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 style={{fontFamily:'unbounded'}}>Unbounded</h1>
+      <h1 style={{fontFamily:'raleway'}}>Raleway</h1>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
+
+      <Header/>
       
     </>
   )
-}
-
-export default App
+};
+export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Info} from './info'
+
+export const Header:React.FC = () =>{
+    return (
+        <header>
+            <div className="content">
+                <Info />
+            </div>            
+        </header>
+    )
+};
