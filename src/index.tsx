d7a0c7abf2dfcc5fd1d2b6b2ec0b65f249ad0814
Oscar <diazoscar088@gmail.com>
2026-03-09 21:16:12 +0100

    feat: create dark theme button functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@/css/index.css'
-import App from './App.tsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@/css/index.css';
+import App from './App.tsx';
 
 const containerRoot = document.querySelector('#root');
-if(!containerRoot){
-  console.error('Root undefined')
-}else{
+if (!containerRoot) {
+  console.error('Root undefined');
+} else {
   const root = createRoot(containerRoot);
   root.render(
-    <StrictMode>
-    <App />
-   </StrictMode>
-  )
+    // <StrictMode>
+    <App />,
+    //  </StrictMode>
+  );
 }
