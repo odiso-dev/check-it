ec8776ec71b436e1c7aab1ebce55fd3edc9f3675
Oscar <diazoscar088@gmail.com>
2026-04-29 19:02:06 +0200

    feat: refactor toast style

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,22 @@ import { Header } from '@/pods/header/header';
 import { Main } from '@/scenes/main/main';
 import { ModalProvider } from '@/context/modal-provider';
 import { ChannelRealtimeProvider } from '@/context/channel-provider';
-import {ModalCategoryProvider} from '@/context/modal-category-provider';
-import {AuthProvider} from '@/context/auth-provider'
+import { ModalCategoryProvider } from '@/context/modal-category-provider';
+import { AuthProvider } from '@/context/auth-provider';
 
 export function App(): React.FC {
-
-
   return (
-      <AuthProvider>
-    <ChannelRealtimeProvider>
-      <ModalProvider>
-      <ModalCategoryProvider>
-        <LayoutApp>
-          <Header />
-          <Main />
-        </LayoutApp>
-      </ModalCategoryProvider>
-      </ModalProvider>
-    </ChannelRealtimeProvider>
+    <AuthProvider>
+      <ChannelRealtimeProvider>
+        <ModalProvider>
+          <ModalCategoryProvider>
+            <LayoutApp>
+              <Header />
+              <Main />
+            </LayoutApp>
+          </ModalCategoryProvider>
+        </ModalProvider>
+      </ChannelRealtimeProvider>
     </AuthProvider>
   );
 }
